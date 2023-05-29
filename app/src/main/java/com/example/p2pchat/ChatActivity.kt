6f1aeb99972e217241fbd3be8162dad5cc4be442
Barış Gökmen <barisgokmen06@gmail.com>
2023-05-30 02:07:34 +0300

    still broken, but no crash. Devices can pair and route to Chat

--- app/src/main/java/com/example/p2pchat/ChatActivity.kt
+++ app/src/main/java/com/example/p2pchat/ChatActivity.kt
@@ -1,121 +1,86 @@
 package com.example.p2pchat
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.IOException
-import java.io.InputStreamReader
-import java.io.PrintWriter
-import java.net.Socket
 
 class ChatActivity : AppCompatActivity() {
 
-    private lateinit var receivedMessagesTextView: TextView
-    private lateinit var inputMessageEditText: EditText
+    private lateinit var messageTextView: TextView
+    private lateinit var messageEditText: EditText
     private lateinit var sendButton: Button
 
-    private lateinit var clientSocket: Socket
-    private lateinit var outputWriter: PrintWriter
-    private lateinit var inputReader: BufferedReader
-
-    companion object {
-        private const val SERVER_PORT = 8888
-        const val EXTRA_IS_GROUP_OWNER = "is_group_owner"
-        const val EXTRA_HOST_ADDRESS = "host_address"
-    }
-
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.test)
 
-        receivedMessagesTextView = findViewById(R.id.receivedMessagesTextView)
-        inputMessageEditText = findViewById(R.id.inputMessageEditText)
+        messageTextView = findViewById(R.id.receivedMessagesTextView)
+        messageEditText = findViewById(R.id.inputMessageEditText)
         sendButton = findViewById(R.id.sendButton)
 
+        val outputWriter: BufferedWriter? = (applicationContext as? DeviceList)?.outputWriter
+        val inputReader: BufferedReader? = (applicationContext as? DeviceList)?.inputReader
+
+
         sendButton.setOnClickListener {
-            val message = inputMessageEditText.text.toString().trim()
-
+            val message = messageEditText.text.toString()
             if (message.isNotEmpty()) {
-                // Send the message to the connected device
-                sendMessage(message)
-
-                // Add the sent message to the chat screen
-                addMessage("Me: $message")
-
-                // Clear the input field
-                inputMessageEditText.text.clear()
+                sendMessage(message, outputWriter)
+                messageEditText.text.clear()
             }
         }
 
-        // Get the connection information from the intent
-        val isGroupOwner = intent.getBooleanExtra(EXTRA_IS_GROUP_OWNER, false)
-        val hostAddress = intent.getStringExtra(EXTRA_HOST_ADDRESS)
-
-        // Initialize the clientSocket and perform network operations in a background thread
         Thread {
-            try {
-                clientSocket = Socket(hostAddress, SERVER_PORT)
-
-                // Initialize the outputWriter and inputReader
-                outputWriter = PrintWriter(clientSocket.getOutputStream(), true)
-                inputReader = BufferedReader(InputStreamReader(clientSocket.getInputStream()))
-
-                // Receive messages from the server
-                var message: String?
-                while (true) {
-                    message = inputReader.readLine()
-                    if (message != null) {
-                        runOnUiThread {
-                            addMessage("Server: $message")
-                        }
-                    }
+            while (true) {
+                val message = readMessage(inputReader)
+                if (message.isEmpty()) {
+                    // Empty message indicates the end of the input stream, so break the loop
+                    break
                 }
-            } catch (e: IOException) {
-                e.printStackTrace()
-            } finally {
-                closeSocketConnection()
+                runOnUiThread {
+                    messageTextView.append("Received: $message\n")
+                }
             }
         }.start()
     }
 
-    private fun sendMessage(message: String) {
-        Thread {
-            try {
-                outputWriter.println(message)
-                outputWriter.flush()
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }.start()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        closeSocketConnection()
-    }
-
-    private fun addMessage(message: String) {
-        receivedMessagesTextView.append("$message\n")
-    }
-
-    private fun closeSocketConnection() {
+    private fun sendMessage(message: String, outputWriter: BufferedWriter?) {
         try {
-            if (::outputWriter.isInitialized) {
-                outputWriter.close()
+            outputWriter?.run {
+                write(message)
+                newLine()
+                flush()
             }
-            if (::inputReader.isInitialized) {
-                inputReader.close()
-            }
-            if (::clientSocket.isInitialized && !clientSocket.isClosed) {
-                clientSocket.close()
+            runOnUiThread {
+                messageTextView.append("Sent: $message\n")
             }
         } catch (e: IOException) {
             e.printStackTrace()
         }
+    }
+
+    private fun readMessage(inputReader: BufferedReader?): String {
+        val message = StringBuilder()
+        try {
+            var line: String? = inputReader?.readLine()
+            while (line != null) {
+                message.append(line)
+                if (inputReader != null) {
+                    line = inputReader.readLine()
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return message.toString()
     }
 }
--- app/src/main/java/com/example/p2pchat/DeviceList.kt
+++ app/src/main/java/com/example/p2pchat/DeviceList.kt
@@ -13,12 +13,18 @@ import android.net.wifi.p2p.WifiP2pManager
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
+import java.io.BufferedWriter
 import java.io.IOException
 import java.net.InetAddress
 import java.net.ServerSocket
 import java.net.Socket
 
 class DeviceList : AppCompatActivity() {
+
+
+    var outputWriter: BufferedWriter? = null
+    var inputReader: BufferedReader? = null
 
     private lateinit var wifiP2pManager: WifiP2pManager
     private lateinit var channel: WifiP2pManager.Channel
@@ -33,14 +39,13 @@ class DeviceList : AppCompatActivity() {
     private lateinit var deviceArrayAdapter: ArrayAdapter<String>
 
     private lateinit var connectionInfo: WifiP2pInfo
-    private lateinit var chatServer: ChatServer
-    private lateinit var chatClient: ChatClient
 
     companion object {
         private const val SERVER_PORT = 8888
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.device_list)
 
@@ -105,165 +110,78 @@ class DeviceList : AppCompatActivity() {
         }
         wifiP2pManager.connect(channel, config, object : WifiP2pManager.ActionListener {
             override fun onSuccess() {
-                showToast("Connection request sent to ${device.deviceName}. Waiting for connection to be established...")
+                showToast("Connection to ${device.deviceName} initiated.")
             }
 
             override fun onFailure(reasonCode: Int) {
-                showToast("Connection to ${device.deviceName} failed. Please try again.")
+                showToast("Connection failed. Please try again.")
             }
         })
-    }
-
-
-
-
-    private fun disconnect(){
-        wifiP2pManager.removeGroup(channel, object : WifiP2pManager.ActionListener{
-            override fun onSuccess() {
-                showToast("Disconnected")
-            }
-
-            override fun onFailure(response: Int) {
-                showToast("Disconnection Failed")
-            }
-        })
-    }
-
-     inner class WiFiDirectBroadcastReceiver : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val action = intent.action
-
-            when (action) {
-                WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
-                    val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
-                    isWifiP2pEnabled = state == WifiP2pManager.WIFI_P2P_STATE_ENABLED
-                }
-                WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
-                    if (isWifiP2pEnabled) {
-                        wifiP2pManager.requestPeers(channel, object : WifiP2pManager.PeerListListener {
-                            override fun onPeersAvailable(peerList: WifiP2pDeviceList) {
-                                devices.clear()
-                                devices.addAll(peerList.deviceList)
-                                deviceArrayAdapter.clear()
-                                for (device in devices) {
-                                    deviceArrayAdapter.add(device.deviceName)
-                                }
-                                deviceArrayAdapter.notifyDataSetChanged()
-                            }
-                        })
-                    }
-                }
-                WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
-                    val networkInfo = intent.getParcelableExtra<NetworkInfo>(WifiP2pManager.EXTRA_NETWORK_INFO)
-                    if (networkInfo?.isConnected == true) {
-                        // We are connected, get connection information
-                        wifiP2pManager.requestConnectionInfo(channel, object : WifiP2pManager.ConnectionInfoListener {
-                            override fun onConnectionInfoAvailable(info: WifiP2pInfo) {
-                                connectionInfo = info
-                                println("\nConnection Info: ${connectionInfo.toString()}")
-                                if (connectionInfo.groupFormed && connectionInfo.isGroupOwner) {
-                                    // We are the group owner, start a chat server
-                                    chatServer = ChatServer()
-                                    chatServer.start()
-                                } else if (connectionInfo.groupFormed) {
-                                    // We are a client, connect to the group owner
-                                    chatClient = ChatClient(connectionInfo.groupOwnerAddress)
-                                    chatClient.start()
-                                }
-
-                            }
-                        })
-                    }
-                }
-
-            }
-        }
     }
 
     private fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
-    private fun closeSocketConnection() {
-        try {
-            chatServer.interrupt()
-            chatClient.interrupt()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-    }
+    inner class WiFiDirectBroadcastReceiver : BroadcastReceiver() {
 
-    private inner class ChatServer : Thread() {
-        private var running = false
-
-        override fun start() {
-            running = true
-            super.start()
-
-        }
-
-        fun stopServer() {
-            running = false
-        }
-
-        override fun run() {
-            var serverSocket: ServerSocket? = null
-            try {
-                serverSocket = ServerSocket(SERVER_PORT)
-                //while (running) {
-                    val socket = serverSocket.accept()
-                    // Handle incoming connections
-                    println("Chat Server Running with IP: ${socket.localAddress.toString()}")
-                    showToast("Chat Server Running with IP: ${socket.localAddress.toString()}")
-                    val intent = Intent(this@DeviceList, ChatActivity::class.java).apply {
-                        putExtra(ChatActivity.EXTRA_IS_GROUP_OWNER, connectionInfo.groupFormed && connectionInfo.isGroupOwner)
-                        putExtra(ChatActivity.EXTRA_HOST_ADDRESS, connectionInfo.groupOwnerAddress?.hostAddress)
-                    }
-                    startActivity(intent)
-                //}
-            } catch (e: IOException) {
-                e.printStackTrace()
-            } finally {
-                serverSocket?.close()
-            }
-        }
-    }
-
-    private inner class ChatClient(private val hostAddress: InetAddress) : Thread() {
-        private var running = false
-
-        override fun start() {
-            running = true
-            super.start()
-        }
-
-        fun stopClient() {
-            running = false
-        }
-
-        override fun run() {
-            var socket: Socket? = null
-            try {
-                socket = Socket(hostAddress, SERVER_PORT)
-                // Handle the connection
-                println("Client Running with local IP: ${socket.localAddress.toString()}")
-                showToast("Client Running with local IP: ${socket.localAddress.toString()}")
-                val intent = Intent(this@DeviceList, ChatActivity::class.java).apply {
-                    putExtra(ChatActivity.EXTRA_IS_GROUP_OWNER, connectionInfo.groupFormed && connectionInfo.isGroupOwner)
-                    putExtra(ChatActivity.EXTRA_HOST_ADDRESS, connectionInfo.groupOwnerAddress?.hostAddress)
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            when (action) {
+                WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION -> {
+                    val state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1)
+                    isWifiP2pEnabled = state == WifiP2pManager.WIFI_P2P_STATE_ENABLED
                 }
-                startActivity(intent)
+                WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION -> {
+                    wifiP2pManager.requestPeers(channel) { peerList: WifiP2pDeviceList ->
+                        devices.clear()
+                        devices.addAll(peerList.deviceList)
 
+                        deviceArrayAdapter.clear()
+                        devices.forEach { device ->
+                            deviceArrayAdapter.add(device.deviceName + "\n" + device.deviceAddress)
+                        }
+                    }
+                }
+                WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION -> {
+                    val networkInfo: NetworkInfo? = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO)
+                    if (networkInfo?.isConnected == true) {
+                        wifiP2pManager.requestConnectionInfo(channel) { info: WifiP2pInfo ->
+                            connectionInfo = info
+                            if (connectionInfo.groupFormed && connectionInfo.isGroupOwner) {
+                                Thread {
+                                    // Start server socket to listen for incoming connections
+                                    val serverSocket = ServerSocket(SERVER_PORT)
+                                    val clientSocket = serverSocket.accept()
 
-            } catch (e: IOException) {
-                e.printStackTrace()
-            } finally {
-                socket?.close()
+                                    outputWriter = clientSocket.getOutputStream().bufferedWriter()
+                                    inputReader = clientSocket.getInputStream().bufferedReader()
+
+                                    // Start activity to handle the chat
+                                    val intent = Intent(context, ChatActivity::class.java)
+                                    startActivity(intent)
+                                }.start()
+                            } else if (connectionInfo.groupFormed) {
+                                Thread {
+                                    // Connect as a client to the group owner
+                                    val hostAddress = connectionInfo.groupOwnerAddress.hostAddress
+                                    val clientSocket = Socket(hostAddress, SERVER_PORT)
+
+                                    outputWriter = clientSocket.getOutputStream().bufferedWriter()
+                                    inputReader = clientSocket.getInputStream().bufferedReader()
+
+                                    // Start activity to handle the chat
+                                    val intent = Intent(context, ChatActivity::class.java)
+                                    startActivity(intent)
+                                }.start()
+                            }
+                        }
+                    }
+                }
+                WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION -> {
+                    // TODO: Handle this action if needed
+                }
             }
         }
     }
-
-
-
-    // Rest of the code for sending/receiving messages, handling data exchange, etc.
 }
